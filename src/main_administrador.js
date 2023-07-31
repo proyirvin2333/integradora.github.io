5c34e0eb58b33b53edb648f027e8fd0800531f83
proyirvin2333 <proyirvin2333@gmail.com>
2023-07-30 21:20:37 -0600

    ya casi

--- src/main_administrador.js
+++ src/main_administrador.js
@@ -1,10 +1,12 @@
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
 import { loginCheck } from "./app/loginCheck.js";
 import { auth } from './app/firebase.js';
+import './app/promocionesForm.js'
+import './app/productosForm.js'
 import './app/loginCheck.js';
 import './app/signupForm.js';
 import './app/logOut.js';
-import './app/productosForm.js'
+
 onAuthStateChanged(auth, async (user) => {
     loginCheck(user)
 })
